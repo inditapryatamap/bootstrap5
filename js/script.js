14898a361f7c7e383bd9d3e2c3456d69458f31b7
infitapryatamap <inditapryatama9@gmail.com>
2023-06-13 14:13:54 +0700

    3th Commit

--- js/script.js
+++ js/script.js
@@ -16,3 +16,32 @@ $(".page-scroll").on("click", function (event) {
 
   event.preventDefault();
 });
+
+// Parallax
+// About
+$(window).on("load", function () {
+  $(".kiri").addClass("kiri-go");
+  $(".kanan").addClass("kanan-go");
+});
+
+// Jumbotron
+$(window).scroll(function () {
+  var windowScroll = $(this).scrollTop();
+
+  $(".jumbotron img").css({
+    transform: "translate(0px, " + windowScroll / 4 + "%)",
+  });
+
+  $(".jumbotron h1").css({
+    transform: "translate(0px, " + windowScroll / 2 + "%)",
+  });
+
+  // portofolio
+  if (windowScroll > $(".portofolio").offset().top - 600) {
+    $(".portofolio .img-thumbnail").each(function (i) {
+      setTimeout(function () {
+        $(".portofolio .img-thumbnail").eq(i).addClass("muncul");
+      }, 300 * (i + 1));
+    });
+  }
+});
